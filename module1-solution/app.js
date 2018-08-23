12b234cea65dd3c7a3a61475975ff68cfebdbf80
DRDS-WIN10-X64-\DrDig <drdigg0r@gmail.com>
2018-08-23 19:54:12 +0200

    Adding requested functionality

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,3 +1,4 @@
+// Wrapping in IIFE (Immediately-invoked Function Expression)
 (function () {
     'use strict';
     
@@ -9,11 +10,19 @@
     function LunchCheckController($scope, $filter) {
 
         $scope.commaSeparatedLunchItemsInput = "";
+        $scope.message = "";
 
         $scope.evaluateMeal = function () {
 
-            var mealItems = $scope.commaSeparatedLunchItemsInput.split(", ");
-            console.log("mealItems = " + mealItems);
+            var mealItems = $scope.commaSeparatedLunchItemsInput.split(",");
+            console.log("mealItems = " + mealItems.length);
+
+            if (mealItems.length <= 3) {
+                $scope.message = "Enjoy!";
+            } else {
+                $scope.message = "Too much!";
+            }
+            
         };
 
     }
